f5df0f37443ebf9964d4f4ded99d35b6e78075bc
TALABI AYOMIDE <flioolawale@gmail.com>
2023-03-07 16:36:33 +0100

    features(add): Added Location Model

---
+++ lib/models/location.dart
@@ -0,0 +1,28 @@
+class Location {
+  final String title;
+  final String imageUrl;
+
+  Location({
+    required this.title,
+    required this.imageUrl,
+  });
+}
+
+List<Location> locations = [
+  Location(
+    title: "Outdoor Getaways",
+    imageUrl: "https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/airbnb%2Fnature.jpeg?alt=media&token=d0dda841-e419-4be0-aa0e-56dcdd42acfd",
+  ),
+  Location(
+    title: "Unique Stays",
+    imageUrl: "https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/airbnb%2Funique.jpeg?alt=media&token=669e01b2-64b9-47e5-a160-e1a8db1c1c7e",
+  ),
+  Location(
+    title: "Outdoor Getaways",
+    imageUrl: "https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/airbnb%2Fnature.jpeg?alt=media&token=d0dda841-e419-4be0-aa0e-56dcdd42acfd",
+  ),
+  Location(
+    title: "Unique Stays",
+    imageUrl: "https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/airbnb%2Funique.jpeg?alt=media&token=669e01b2-64b9-47e5-a160-e1a8db1c1c7e",
+  ),
+];
